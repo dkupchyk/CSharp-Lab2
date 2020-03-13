882de5b1647f1fce2b2f7d942505c9d56290bce5
dkupchyk <51787088+dkupchyk@users.noreply.github.com>
2020-03-13 20:58:05 +0200

    Lab 3

---
+++ View/UserView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kupchyk01
+{
+    /// <summary>
+    /// Interaction logic for UserControl.xaml
+    /// </summary>
+    public partial class UserView : UserControl
+    {
+        private UserViewModel _viewModel;
+        public UserView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new UserViewModel();
+        }
+        
+    }
+}
